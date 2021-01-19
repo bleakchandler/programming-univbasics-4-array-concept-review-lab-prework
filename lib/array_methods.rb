ff42e31b89349c3d9e7d498d90596f7ed28725d5
Bleak Chandler <bleakchandler@gmail.com>
2021-01-19 22:12:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,10 +3,9 @@ def find_element_index(array, value_to_find)
   value = value_to_find
  
  puts newarray[value]
-  while newarray[value] do
-     newarray.index(value)
+ 
     
- end
+ 
 end
 
 def find_max_value(array)
